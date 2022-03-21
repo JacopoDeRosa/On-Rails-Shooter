11d3a677299054d768d2966d22ca2b47254bdc4a
Jack <jacopo.derosa00@gmail.com>
2022-03-21 15:10:11 +0100

    Finished Hand Cannon Script

--- Assets/Scripts/HandCannon.cs
+++ Assets/Scripts/HandCannon.cs
@@ -5,44 +5,72 @@ using UnityEngine.Events;
 
 public class HandCannon : MonoBehaviour
 {
-    [SerializeField] private float _roundPerMinute;
-    [SerializeField] private Transform _barrels;
+    [SerializeField] private float _roundsPerMinute;
+    [SerializeField] private float _windUpTime;
+    [SerializeField] private float _windDownTime;
     [SerializeField] private bool _firing = false;
+    [SerializeField] private Transform _barrels;
     [SerializeField] private ParticleSystem _bullet;
-    public UnityEvent onFire;
 
+    [SerializeField]
+    private float _currentSpeed = 0;
 
-    private float _firingTimer = 0;
-    private float FireInterval { get => 60 / _roundPerMinute; }
+    private float RoundsPerSecond { get => 60 / _roundsPerMinute; }
+    private float RotationSpeed { get => _roundsPerMinute; }
+    private bool BarrelAligned 
+    {
+        get
+        {
+            float rot = _barrels.rotation.eulerAngles.z % 60;
+
+            return rot > 60 - _currentSpeed * Time.fixedDeltaTime && rot < 60;
+        }
+    }
+    private float WindUpSpeed { get => RotationSpeed / _windUpTime; }
+    private float WindDownSpeed { get => RotationSpeed / _windDownTime; }
 
     public void StartFiring()
     {
         _firing = true;
     }
-
     public void StopFiring()
     {
         _firing = false;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        if(_firing)
+        if (_firing)
         {
-            // Each revolution = 6 shots
-            // For each shot it needs to rotate 60°
-            // 60 RPM = 60 degrees per second;
-            // 120 RPM = 120 degrees per second;
-            _barrels.Rotate(new Vector3(0, 0, _roundPerMinute * Time.deltaTime), Space.Self);
-
-            if (_firingTimer >= FireInterval)
+            if (_currentSpeed < RotationSpeed)
             {
-                _firingTimer = 0;
+                _currentSpeed += WindUpSpeed * Time.fixedDeltaTime;
+            }
+            else
+            {
+                _currentSpeed = RotationSpeed;
+            }
+            if (_currentSpeed == RotationSpeed && BarrelAligned)
+            {
                 _bullet.Play();
-                onFire.Invoke();
             }
         }
-
-        _firingTimer += Time.deltaTime;
+        else
+        {
+            if(_currentSpeed > 0)
+            {
+                _currentSpeed -= WindDownSpeed * Time.fixedDeltaTime;
+            }
+            else
+            {
+                _currentSpeed = 0;
+            }
+        }
+        if (_currentSpeed > 0)
+        {
+            _barrels.Rotate(new Vector3(0, 0, _currentSpeed * Time.fixedDeltaTime), Space.Self);
+        }
     }
+
+
 }
