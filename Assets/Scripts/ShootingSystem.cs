d975f5093ee4e6b6bd95f1a24793cf05a5d40d8c
Jack <jacopo.derosa00@gmail.com>
2022-03-21 10:21:53 +0100

    Added Hand Cannons and Reworked input events

--- Assets/Scripts/ShootingSystem.cs
+++ Assets/Scripts/ShootingSystem.cs
@@ -6,34 +6,57 @@ using UnityEngine.InputSystem;
 public class ShootingSystem : MonoBehaviour
 {
     [SerializeField] private Vector2 _mousePosition;
-    [SerializeField] private ParticleSystem _bullets;
+    [SerializeField] private Transform _leftArm;
+    [SerializeField] private Transform _rightArm;
+    [SerializeField] private ParticleSystem[] _bullets;
     [SerializeField] private float _zeroDistance;
+    [SerializeField] private PlayerInput _input;
 
-    public void OnMousePosition(InputAction.CallbackContext context)
+    private void Start()
+    {
+        if (_input == null) _input = FindObjectOfType<PlayerInput>();
+        _input.actions["Fire"].started += OnFireDown;
+        _input.actions["Fire"].canceled += OnFireUp;
+
+        _input.actions["Mouse Position"].performed += OnMousePosition;
+    }
+
+
+
+    private void OnMousePosition(InputAction.CallbackContext context)
     {
         _mousePosition = context.ReadValue<Vector2>();
+        Vector3 aimPoint = FindAimPoint();
+        _leftArm.LookAt(aimPoint);
+        _rightArm.LookAt(aimPoint);
+       
     }
-    
-    public void OnShoot(InputAction.CallbackContext context)
+    private void OnFireDown(InputAction.CallbackContext context)
     {
-        if(context.started)
+        foreach (ParticleSystem system in _bullets)
         {
-            Vector3 aimPoint = Vector3.zero;
-            Ray ray = Camera.main.ScreenPointToRay(_mousePosition);
-            if(Physics.Raycast(ray, out RaycastHit hit))
-            {
-                aimPoint = hit.point;
-            }
-            else
-            {
-                aimPoint = ray.GetPoint(_zeroDistance);
-            }
-            _bullets.transform.LookAt(aimPoint);
-            _bullets.Play();
+            system.Play();
         }
     }
+    private void OnFireUp(InputAction.CallbackContext context)
+    {
+        print("Mouse Up");
+    }
 
-
+    private Vector3 FindAimPoint()
+    {
+        Vector3 aimPoint = Vector3.zero;
+        Ray ray = Camera.main.ScreenPointToRay(_mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            aimPoint = hit.point;
+        }
+        else
+        {
+            aimPoint = ray.GetPoint(_zeroDistance);
+        }
+        return aimPoint;
+    }
   
 
 }
