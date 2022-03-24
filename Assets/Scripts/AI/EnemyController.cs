c777d6b8833dfc7f3e4a2e58c6e50718f5e66ccf
Jack <jacopo.derosa00@gmail.com>
2022-03-24 12:03:05 +0100

    Added enemy group controller.

--- Assets/Scripts/AI/EnemyController.cs
+++ Assets/Scripts/AI/EnemyController.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
+    [SerializeField] private NavMeshAgent _navmeshAgent;
     [SerializeField] private bool _startsAwake;
+    [SerializeField] private bool _canGiveChase;
+    [SerializeField] private Transform _target;
+
+    private bool _giveChase = false;
+    
 
     private void Start()
     {
@@ -18,9 +25,22 @@ public class EnemyController : MonoBehaviour
     {
         _animator.SetTrigger("Wake");
     }
-
     public void Die()
     {
-        _animator.SetTrigger("Die " + Random.Range(1, 3));
+       _animator.SetTrigger("Die " + Random.Range(1, 3));
+       _navmeshAgent.isStopped = true;
+    }
+    public void Chase()
+    {
+        if (_canGiveChase == false) return;
+        _giveChase = true;
+    }
+
+    private void Update()
+    {
+        if(_giveChase)
+        {
+            _navmeshAgent.SetDestination(_target.position);
+        }
     }
 }
---
+++ Assets/Scripts/AI/EnemyGroup.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnemyGroup : MonoBehaviour
+{
+    [SerializeField] private List<EnemyController> _enemies;
+    public UnityEvent onAllDead;
+
+    private int _deadEnemies;
+
+    private void Start()
+    {
+        foreach(var enemy in _enemies)
+        {
+          var health = enemy.GetComponent<Health>();
+            if(health)
+            {
+                health.onDeath.AddListener(OnEnemyDeath);
+            }
+        }
+    }
+
+    public void WakeUpAll()
+    {
+        foreach (var enemy in _enemies)
+        {
+            enemy.WakeUp();
+        }
+    }
+
+    private void OnEnemyDeath()
+    {
+        _deadEnemies++;
+        if (_deadEnemies == _enemies.Count)
+        {
+            onAllDead.Invoke();
+            print("All enemies of " + name + " are dead.");
+        }
+    }
+}
